096605c3ecd9f74981c231c077c9b3ba73017b8d
Joseph Jide <josephjidejw@gmail.com>
2024-10-08 08:50:20 +0100

    jhghjsd

--- src/pages/Bets/game/index.tsx
+++ src/pages/Bets/game/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Suspense, lazy, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
@@ -44,7 +44,6 @@ const GamePage: React.FC = () => {
   const userId = Cookies.get('userId');
   const [odds, setOdds] = useState<Odds | null>(null);
 
-  // Set match action based on the query parameter
   useEffect(() => {
     if (action === 'BULL' || action === 'BEAR') {
       setMatchAction(action as 'BULL' | 'BEAR');
@@ -52,31 +51,12 @@ const GamePage: React.FC = () => {
   }, [action]);
 
   const handleDurationChange = (selectedDuration: string) => {
-    //const formattedDuration = formatDuration(selectedDuration);
     setDuration(selectedDuration);
   };
-  
-  const formatDuration = (duration: string): string => {
-    if (duration.includes('minutes')) {
-      return duration.replace(' minutes', 'm');
-    } else if (duration.includes('hour')) {
-      return duration.replace(' hour', 'h').replace('s', '');
-    } else if (duration.includes('hours')) {
-      return duration.replace(' hours', 'h');
-    }
-    return duration; // Fallback in case of other formats
-  };
-
-  const handleOddSelection = (oddType: 'home' | 'draw' | 'away') => {
-    setSelectedOdd(oddType);
-  };
-
-
-  // Get the current user's ID from cookies (Assuming it's already set somewhere)
 
   const fetchOdds = useCallback(async () => {
     try {
-      setLoading(true); // Set loading to true before fetching
+      setLoading(true); 
       const response = await fetch(`${apiUrl}/api/getOdds`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -87,18 +67,21 @@ const GamePage: React.FC = () => {
           bull_or_bear: matchAction,
         }),
       });
+
+      if (!response.ok) {
+        throw new Error('Match not found');
+      }
+
       const data = await response.json();
-      console.log('odd data', data)
-      setOdds(data); // Assume data contains odds for home, draw, away
-      setLoading(false); // Stop loading after fetching
+      setOdds(data);
+      setLoading(false);
     } catch (error) {
-      console.log('error', error)
+      console.error('error', error);
       setErrorMessage('Failed to load odds. Please try again later.');
-      setLoading(false); // Stop loading even if there's an error
+      setLoading(false);
     }
   }, [match_id, userId, duration, matchAction]);
 
-  // Call fetchOdds when duration changes
   useEffect(() => {
     if (duration) {
       fetchOdds();
@@ -107,7 +90,6 @@ const GamePage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center min-h-screen overflow-x-hidden">
-      {/* Match Information */}
       <div className="flex justify-center items-center space-x-4 my-8">
         <div className="flex items-center space-x-2">
           <Image src={token1Logo as string} alt={token1Symbol as string} width={70} height={70} />
@@ -120,27 +102,19 @@ const GamePage: React.FC = () => {
         </div>
       </div>
 
-      {/* Bull/Bear Indicator */}
       {matchAction && (
-        <div
-          className={`absolute top-4 right-4 px-2 py-2 rounded-lg text-white text-xs font-bold ${
-            matchAction === 'BULL' ? 'bg-green-500' : 'bg-red-500'
-          }`}
-        >
+        <div className={`absolute top-4 right-4 px-2 py-2 rounded-lg text-white text-xs font-bold ${matchAction === 'BULL' ? 'bg-green-500' : 'bg-red-500'}`}>
           {matchAction}
         </div>
       )}
 
-      {/* Duration Selection */}
       <div className="my-6  w-full">
         <h3 className="text-lg font-semibold">Select Duration</h3>
         <div className="flex space-x-4 mt-4 overflow-x-auto whitespace-nowrap">
           {['15m', '30m', '1h', '4h', '6h', '24h'].map((d) => (
             <button
               key={d}
-              className={`px-6 py-2 border rounded-lg ${
-                duration === d ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'
-              }`}
+              className={`px-6 py-2 border rounded-lg ${duration === d ? 'bg-green-500 text-white' : 'bg-gray-200 text-black'}`}
               onClick={() => handleDurationChange(d)}
             >
               {d}
@@ -149,44 +123,44 @@ const GamePage: React.FC = () => {
         </div>
       </div>
 
-      {/* Odds Section */}
-      {duration && odds && (
+      {duration && (
         <div className="mt-8 w-full px-4">
-          {['volume', 'trades', 'price', 'marketcap'].map((metric) => (
-            <div key={metric} className="mt-6 w-full px-4">
-              <h3 className="text-xl font-bold capitalize">
-                {matchAction === 'BULL'
-                  ? `Percentage ${metric} Increase`
-                  : `Percentage ${metric} Decrease`} in the next {duration}
-              </h3>
-              <div className="mt-6 space-y-4">
-                <div className="flex justify-between">
-                  <span className="text-sm">Home</span>
-                  <span className="text-sm">Draw</span>
-                  <span className="text-sm">Away</span>
-                </div>
-
-                {loading ? (
-                  <div className="flex justify-between animate-pulse">
-                    <div className="bg-gray-200 h-6 w-12"></div>
-                    <div className="bg-gray-200 h-6 w-12"></div>
-                    <div className="bg-gray-200 h-6 w-12"></div>
-                  </div>
-                ) : (
+          {odds ? (
+            ['volume', 'trades', 'price', 'marketcap'].map((metric) => (
+              <div key={metric} className="mt-6 w-full px-4">
+                <h3 className="text-xl font-bold capitalize">
+                  {matchAction === 'BULL' ? `Percentage ${metric} Increase` : `Percentage ${metric} Decrease`} in the next {duration}
+                </h3>
+                <div className="mt-6 space-y-4">
                   <div className="flex justify-between">
-                    <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds].token_a_odds}</span>
-                    <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds].draw_odds}</span>
-                    <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds].token_b_odds}</span>
+                    <span className="text-sm">Home</span>
+                    <span className="text-sm">Draw</span>
+                    <span className="text-sm">Away</span>
                   </div>
-                )}
+                  {loading ? (
+                    <div className="flex justify-between animate-pulse">
+                      <div className="bg-gray-200 h-6 w-12"></div>
+                      <div className="bg-gray-200 h-6 w-12"></div>
+                      <div className="bg-gray-200 h-6 w-12"></div>
+                    </div>
+                  ) : (
+                    <div className="flex justify-between">
+                      <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds]?.token_a_odds ?? 'No odds'}</span>
+                      <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds]?.draw_odds ?? 'No odds'}</span>
+                      <span className="text-sm text-gray-500 font-bold">{odds[metric as keyof Odds]?.token_b_odds ?? 'No odds'}</span>
+                    </div>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No odds available</p>
+          )}
         </div>
       )}
     </div>
   );
 };
 
-
 export default GamePage;
+
